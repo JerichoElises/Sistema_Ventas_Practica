a70c944479a1833e1de9a0bed2bb4b52ddd41974
Jericho Elises <jerichoelises27@gmail.com>
2025-09-11 13:31:10 -0500

    Agregar archivos de proyecto.

---
+++ SV_Repository/DB/Conexion.cs
@@ -0,0 +1,24 @@
+﻿
+
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace SV_Repository.DB
+{
+    public class Conexion
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _cadenaSql;
+
+        public Conexion(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _cadenaSql = _configuration.GetConnectionString("CadenaSql")!;
+        }
+
+        public SqlConnection Obtener()
+        {
+            return new SqlConnection("_cadenaSql");
+        }
+    }
+}
